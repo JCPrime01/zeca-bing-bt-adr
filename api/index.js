acb3ba36e4c2e21f043f0479a49db9d21f26a721
JCPrime01 <vanzokiller@gmail.com>
2026-05-13 11:47:31 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=LMG1ZWV0ZSJ&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=QXT19M94E6J&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
