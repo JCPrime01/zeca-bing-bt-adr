de42664d6f7500b6cadd13a10819efb234e11bab
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 09:53:32 -0300

    chore: update shareCode to MTYLBOBQNMC

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=57OV94OF1O4&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=MTYLBOBQNMC&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
