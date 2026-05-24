aa057184c1724556f8ebb0a301113c8e02b16456
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-24 09:36:46 -0300

    chore: update shareCode to IAF0E4EP2WL

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=E3VWQJTBMGM&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=IAF0E4EP2WL&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
