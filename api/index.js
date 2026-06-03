282da12eb005019a91049f1f3bbf1beaa357f9ca
JCPrime01 <vanzokiller@gmail.com>
2026-06-03 11:46:35 -0300

    chore: shareCode PJDVWA9FYL6 -> O3CU0P8DF7I (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=PJDVWA9FYL6&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=O3CU0P8DF7I&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
