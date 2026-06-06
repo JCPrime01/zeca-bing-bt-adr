c15cf5ba40a16d104bbf7df3bcc4eec81431d228
JCPrime01 <vanzokiller@gmail.com>
2026-06-06 09:51:55 -0300

    chore: shareCode M1VOMTBCIHI -> 9NWFDCM1CWL (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=M1VOMTBCIHI&afp8=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=9NWFDCM1CWL&afp8=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
