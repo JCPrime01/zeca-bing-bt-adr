fd369e1d45602d88cbbb494ba25d17cecd7abc6d
JCPrime01 <vanzokiller@gmail.com>
2026-07-18 12:48:13 -0300

    chore: shareCode M76WI1QYMGE -> MFJZQXD6J14 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=M76WI1QYMGE&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=MFJZQXD6J14&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
