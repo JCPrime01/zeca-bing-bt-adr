8682ae9a1cf9d7b1cf1d8bcf780a337a03d2c2ac
JCPrime01 <vanzokiller@gmail.com>
2026-07-27 09:35:46 -0300

    chore: shareCode PDPZFX2L86H -> GV8MFFIJ1Z1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=PDPZFX2L86H&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=GV8MFFIJ1Z1&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
