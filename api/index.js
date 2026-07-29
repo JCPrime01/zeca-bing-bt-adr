7945fde1350623cc7891f012fe2835605c5de8c0
JCPrime01 <vanzokiller@gmail.com>
2026-07-29 11:23:47 -0300

    chore: shareCode NOUU9MOMJSD -> R8FQNWTREUH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=NOUU9MOMJSD&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=R8FQNWTREUH&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
