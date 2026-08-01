8d12b336983440f0f6b9cdbf123b4c73f16500bd
JCPrime01 <vanzokiller@gmail.com>
2026-08-01 10:21:32 -0300

    chore: shareCode 9V45M4GIP2G -> XY65Z9L0QDN (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=9V45M4GIP2G&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=XY65Z9L0QDN&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
