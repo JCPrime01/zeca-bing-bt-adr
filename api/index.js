44b66b7e3b12082e358f3767c439f34bd5b94f04
JCPrime01 <vanzokiller@gmail.com>
2026-08-07 10:51:41 -0300

    chore: shareCode VK9JWF0GTM7 -> VPQYHMA79GJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=VK9JWF0GTM7&afp2=bingo&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=VPQYHMA79GJ&afp2=bingo&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
